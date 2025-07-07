8846ee765b2c80b90f58f398dba81dffb294ffff
Bantyn <patelbanty1260@gmail.com>
2025-07-07 23:32:51 +0530

    done

--- globe-gateways/src/App.js
+++ globe-gateways/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Main from '../src/components/main'
 import "../src/assets/css/style.css"
+import {Routes,Route} from 'react-router-dom'
 
 export default function App() {
   return (
     <>
-    <Main></Main>
+    <Routes>
+      <Route path='/' element={<Main></Main>}></Route>
+    </Routes>
     </>
   )
 }
