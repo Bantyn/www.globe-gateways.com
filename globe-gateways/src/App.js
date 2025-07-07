1d4fc5d404c8322a2b2aa1f85a3eea3a7acf0638
Bantyn <patelbanty1260@gmail.com>
2025-07-07 23:21:27 +0530

    App Created | Day 1 | 7/7/2025 Monday

---
+++ globe-gateways/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../src/components/main'
+import "../src/assets/css/style.css"
+
+export default function App() {
+  return (
+    <>
+    <Main></Main>
+    </>
+  )
+}
