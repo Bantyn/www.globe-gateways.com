92beecfa1b381b8d3954ab28ac5871f855ce2890
Bantyn <patelbanty1260@gmail.com>
2025-07-08 22:20:23 +0530

    updated

--- globe-gateways-main/src/App.jsx
+++ globe-gateways-main/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Main from './components/main'; 
+import Main from './components/main.jsx'; 
+import Navbar from './components/navbar.jsx'; 
 import './assets/css/style.css';
 import './assets/js/main.js'; 
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/navbar' element={<Navbar />} />
     </Routes>
   );
 }
